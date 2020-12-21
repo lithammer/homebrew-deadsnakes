8f509d17046a946837af26dbeba73054f413f348
Peter Lithammer <peter.lithammer@gmail.com>
2020-12-21 09:35:51 +0100

    python@3.6: Bump revision to rebuild bottles

--- Formula/python@3.6.rb
+++ Formula/python@3.6.rb
@@ -4,7 +4,7 @@ class PythonAT36 < Formula
   url "https://www.python.org/ftp/python/3.6.12/Python-3.6.12.tar.xz"
   sha256 "70953a9b5d6891d92e65d184c3512126a15814bee15e1eff2ddcce04334e9a99"
   license "Python-2.0"
-  revision 3
+  revision 4
 
   livecheck do
     url "https://www.python.org/ftp/python/"
