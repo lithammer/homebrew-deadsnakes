ecd82df68fc13f755b3d1d297e047be4a8ecbab7
Peter Lithammer <peter.lithammer@gmail.com>
2020-12-21 12:23:08 +0100

    python@2.7: Port formula fixes from upstream
    Closes #32.
    
    Signed-off-by: Peter Lithammer <177685+lithammer@users.noreply.github.com>

--- Formula/python@2.7.rb
+++ Formula/python@2.7.rb
@@ -3,7 +3,13 @@ class PythonAT27 < Formula
   homepage "https://www.python.org/"
   url "https://www.python.org/ftp/python/2.7.18/Python-2.7.18.tgz"
   sha256 "da3080e3b488f648a3d7a4560ddee895284c3380b11d6de75edb986526b9a814"
-  revision 4
+  license "Python-2.0"
+  revision 5
+
+  livecheck do
+    url "https://www.python.org/ftp/python/"
+    regex(%r{href=.*?v?(2\.7(?:\.\d+)*)/?["' >]}i)
+  end
 
   bottle do
     root_url "https://github.com/lithammer/homebrew-deadsnakes/releases/download/python@2.7-2.7.18_4"
@@ -14,12 +20,14 @@ class PythonAT27 < Formula
   # setuptools remembers the build flags python is built with and uses them to
   # build packages later. Xcode-only systems need different flags.
   pour_bottle? do
-    reason <<~EOS
-      The bottle needs the Apple Command Line Tools to be installed.
-        You can install them, if desired, with:
-          xcode-select --install
-    EOS
-    satisfy { MacOS::CLT.installed? }
+    on_macos do
+      reason <<~EOS
+        The bottle needs the Apple Command Line Tools to be installed.
+          You can install them, if desired, with:
+            xcode-select --install
+      EOS
+      satisfy { MacOS::CLT.installed? }
+    end
   end
 
   keg_only :versioned_formula
@@ -37,8 +45,23 @@ class PythonAT27 < Formula
   uses_from_macos "unzip"
   uses_from_macos "zlib"
 
-  skip_clean "bin/pip2", "bin/pip-2.7"
-  skip_clean "bin/easy_install2", "bin/easy_install-2.7"
+  skip_clean "bin/pip2", "bin/pip-2.6"
+  skip_clean "bin/easy_install2", "bin/easy_install-2.6"
+
+  link_overwrite "bin/2to3"
+  link_overwrite "bin/idle2"
+  link_overwrite "bin/pip2"
+  link_overwrite "bin/pydoc2"
+  link_overwrite "bin/python2"
+  link_overwrite "bin/python-config2"
+  link_overwrite "bin/wheel2"
+  link_overwrite "share/man/man1/python2.1"
+  link_overwrite "lib/pkgconfig/python2.pc"
+  link_overwrite "lib/pkgconfig/python2-embed.pc"
+  link_overwrite "Frameworks/Python.framework/Headers"
+  link_overwrite "Frameworks/Python.framework/Python"
+  link_overwrite "Frameworks/Python.framework/Resources"
+  link_overwrite "Frameworks/Python.framework/Versions/Current"
 
   resource "setuptools" do
     url "https://files.pythonhosted.org/packages/b0/f3/44da7482ac6da3f36f68e253cb04de37365b3dba9036a3c70773b778b485/setuptools-44.0.0.zip"
@@ -55,27 +78,50 @@ class PythonAT27 < Formula
     sha256 "aaef9b8c36db72f8bf7f1e54f85f875c4d466819940863ca0b3f3f77f0a1646f"
   end
 
+  def lib_cellar
+    on_macos do
+      return prefix/"Frameworks/Python.framework/Versions/#{version.major_minor}/lib/python#{version.major_minor}"
+    end
+    on_linux do
+      return prefix/"lib/python#{version.major_minor}"
+    end
+  end
+
+  def site_packages_cellar
+    lib_cellar/"site-packages"
+  end
+
+  # The HOMEBREW_PREFIX location of site-packages.
+  def site_packages
+    HOMEBREW_PREFIX/"lib/python#{version.major_minor}/site-packages"
+  end
+
   def install
     # Unset these so that installing pip and setuptools puts them where we want
     # and not into some other Python the user has installed.
     ENV["PYTHONHOME"] = nil
     ENV["PYTHONPATH"] = nil
 
-    xy = (buildpath/"configure.ac").read.slice(/PYTHON_VERSION, (2\.\d)/, 1)
-    lib_cellar = prefix/"Frameworks/Python.framework/Versions/#{xy}/lib/python#{xy}"
-
     args = %W[
       --prefix=#{prefix}
       --enable-ipv6
       --datarootdir=#{share}
       --datadir=#{share}
-      --enable-framework=#{frameworks}
-      --without-ensurepip
     ]
 
-    cflags   = []
-    ldflags  = []
-    cppflags = []
+    on_macos do
+      args << "--enable-framework=#{frameworks}"
+    end
+    on_linux do
+      args << "--enable-shared"
+      # Required for the _ctypes module
+      # see https://github.com/Linuxbrew/homebrew-core/pull/1007#issuecomment-252421573
+      args << "--with-system-ffi"
+    end
+
+    cflags   = ["-I#{HOMEBREW_PREFIX}/include"]
+    ldflags  = ["-L#{HOMEBREW_PREFIX}/lib"]
+    cppflags = ["-I#{HOMEBREW_PREFIX}/include"]
 
     if MacOS.sdk_path_if_needed
       # Help Python's build system (setuptools/pip) to build things on SDK-based systems
@@ -128,28 +174,35 @@ class PythonAT27 < Formula
     ENV.deparallelize do
       # Tell Python not to install into /Applications (default for framework builds)
       system "make", "install", "PYTHONAPPSDIR=#{prefix}"
-      system "make", "frameworkinstallextras", "PYTHONAPPSDIR=#{pkgshare}"
+      on_macos do
+        system "make", "frameworkinstallextras", "PYTHONAPPSDIR=#{pkgshare}"
+      end
     end
 
-    # Fixes setting Python build flags for certain software
-    # See: https://github.com/Homebrew/homebrew/pull/20182
-    # https://bugs.python.org/issue3588
-    inreplace lib_cellar/"config/Makefile" do |s|
-      s.change_make_var! "LINKFORSHARED",
-        "-u _PyMac_Error $(PYTHONFRAMEWORKINSTALLDIR)/Versions/$(VERSION)/$(PYTHONFRAMEWORK)"
-    end
+    # Any .app get a " 2" attached, so it does not conflict with python 3.x.
+    Dir.glob("#{prefix}/*.app") { |app| mv app, app.sub(/\.app$/, " 2.app") }
 
-    # Prevent third-party packages from building against fragile Cellar paths
-    inreplace [lib_cellar/"_sysconfigdata.py",
-               lib_cellar/"config/Makefile",
-               frameworks/"Python.framework/Versions/Current/lib/pkgconfig/python-2.7.pc"],
-              prefix, opt_prefix
+    on_macos do
+      # Prevent third-party packages from building against fragile Cellar paths
+      inreplace [lib_cellar/"_sysconfigdata.py",
+                 lib_cellar/"config/Makefile",
+                 frameworks/"Python.framework/Versions/Current/lib/pkgconfig/python-2.7.pc"],
+                prefix, opt_prefix
+
+      # Fixes setting Python build flags for certain software
+      # See: https://github.com/Homebrew/homebrew/pull/20182
+      # https://bugs.python.org/issue3588
+      inreplace lib_cellar/"config/Makefile" do |s|
+        s.change_make_var! "LINKFORSHARED",
+          "-u _PyMac_Error $(PYTHONFRAMEWORKINSTALLDIR)/Versions/$(VERSION)/$(PYTHONFRAMEWORK)"
+      end
+    end
 
     # Symlink the pkgconfig files into HOMEBREW_PREFIX so they're accessible.
-    (lib/"pkgconfig").install_symlink Dir["#{frameworks}/Python.framework/Versions/#{xy}/lib/pkgconfig/*"]
+    (lib/"pkgconfig").install_symlink Dir["#{frameworks}/Python.framework/Versions/#{version.major_minor}/lib/pkgconfig/*"]
 
     # Remove the site-packages that Python created in its Cellar.
-    (prefix/"Frameworks/Python.framework/Versions/#{xy}/lib/python#{xy}/site-packages").rmtree
+    site_packages_cellar.rmtree
 
     %w[setuptools pip wheel].each do |r|
       (libexec/r).install resource(r)
@@ -174,14 +227,10 @@ class PythonAT27 < Formula
   def post_install
     ENV.delete "PYTHONPATH"
 
-    xy = (prefix/"Frameworks/Python.framework/Versions").children.min.basename.to_s
-    site_packages = HOMEBREW_PREFIX/"lib/python#{xy}/site-packages"
-    site_packages_cellar = prefix/"Frameworks/Python.framework/Versions/#{xy}/lib/python#{xy}/site-packages"
-
     # Fix up the site-packages so that user-installed Python software survives
     # minor updates, such as going from 2.7.0 to 2.7.1:
 
-    # Create a site-packages in HOMEBREW_PREFIX/lib/python#{xy}/site-packages
+    # Create a site-packages in HOMEBREW_PREFIX/lib/python#{version.major_minor}/site-packages
     site_packages.mkpath
 
     # Symlink the prefix site-packages into the cellar.
@@ -199,23 +248,57 @@ class PythonAT27 < Formula
     rm_rf Dir["#{site_packages}/setuptools*"]
     rm_rf Dir["#{site_packages}/distribute*"]
     rm_rf Dir["#{site_packages}/pip[-_.][0-9]*", "#{site_packages}/pip"]
+    rm_rf Dir["#{site_packages}/wheel*"]
 
-    %w[setuptools pip wheel].each do |pkg|
-      (libexec/pkg).cd do
-        system bin/"python2", "-s", "setup.py", "--no-user-cfg", "install",
-               "--force", "--verbose", "--install-scripts=#{bin}",
-               "--install-lib=#{site_packages}",
-               "--single-version-externally-managed",
-               "--record=installed.txt"
+    system bin/"python2", "-m", "ensurepip"
+
+    # Get set of ensurepip-installed files for later cleanup
+    ensurepip_files = Set.new(Dir["#{site_packages}/setuptools-*"]) +
+                      Set.new(Dir["#{site_packages}/pip-*"]) +
+                      Set.new(Dir["#{site_packages}/wheel-*"])
+
+    # Remove Homebrew distutils.cfg if it exists, since it prevents the subsequent
+    # pip install command from succeeding (it will be recreated afterwards anyways)
+    rm_f lib_cellar/"distutils/distutils.cfg"
+
+    # Install desired versions of setuptools, pip, wheel using the version of
+    # pip bootstrapped by ensurepip
+    system bin/"python2", "-m", "pip", "install", "-v", "--global-option=--no-user-cfg",
+           "--install-option=--force",
+           "--install-option=--single-version-externally-managed",
+           "--install-option=--record=installed.txt",
+           "--upgrade",
+           "--target=#{site_packages}",
+           libexec/"setuptools",
+           libexec/"pip",
+           libexec/"wheel"
+
+    # Get set of files installed via pip install
+    pip_files = Set.new(Dir["#{site_packages}/setuptools-*"]) +
+                Set.new(Dir["#{site_packages}/pip-*"]) +
+                Set.new(Dir["#{site_packages}/wheel-*"])
+
+    # Clean up the bootstrapped copy of setuptools/pip provided by ensurepip.
+    # Also consider the corner case where our desired version of tools is
+    # the same as those provisioned via ensurepip. In this case, don't clean
+    # up, or else we'll have no working setuptools, pip, wheel
+    if pip_files != ensurepip_files
+      ensurepip_files.each do |dir|
+        rm_rf dir
       end
     end
+
+    # pip install with --target flag will just place the bin folder into the
+    # target, so move its contents into the appropriate location
+    mv (site_packages/"bin").children, bin
+    rmdir site_packages/"bin"
 
     rm_rf [bin/"pip", bin/"easy_install"]
     mv bin/"wheel", bin/"wheel2"
 
     # Install unversioned symlinks in libexec/bin.
     {
-      "easy_install" => "easy_install-#{xy}",
+      "easy_install" => "easy_install-#{version.major_minor}",
       "pip"          => "pip2",
       "wheel"        => "wheel2",
     }.each do |unversioned_name, versioned_name|
@@ -223,7 +306,7 @@ class PythonAT27 < Formula
     end
 
     # post_install happens after link
-    %W[python python2 python#{xy}].each do |e|
+    %W[python python2 python#{version.major_minor}].each do |e|
       (HOMEBREW_PREFIX/"bin").install_symlink bin/e
     end
 
@@ -233,7 +316,7 @@ class PythonAT27 < Formula
     library_dirs = [HOMEBREW_PREFIX/"lib", Formula["openssl@1.1"].opt_lib,
                     Formula["sqlite"].opt_lib]
 
-    cfg = prefix/"Frameworks/Python.framework/Versions/#{xy}/lib/python#{xy}/distutils/distutils.cfg"
+    cfg = lib_cellar/"distutils/distutils.cfg"
 
     cfg.atomic_write <<~EOS
       [install]
@@ -245,8 +328,6 @@ class PythonAT27 < Formula
   end
 
   def sitecustomize
-    xy = (prefix/"Frameworks/Python.framework/Versions").children.min.basename.to_s
-
     <<~EOS
       # This file is created by Homebrew and is executed on each python startup.
       # Don't print from here, or else python command line scripts may fail!
@@ -266,52 +347,56 @@ class PythonAT27 < Formula
       # Only do this for a brewed python:
       if os.path.realpath(sys.executable).startswith('#{rack}'):
           # Shuffle /Library site-packages to the end of sys.path
-          library_site = '/Library/Python/#{xy}/site-packages'
+          library_site = '/Library/Python/#{version.major_minor}/site-packages'
           library_packages = [p for p in sys.path if p.startswith(library_site)]
           sys.path = [p for p in sys.path if not p.startswith(library_site)]
           # .pth files have already been processed so don't use addsitedir
           sys.path.extend(library_packages)
           # the Cellar site-packages is a symlink to the HOMEBREW_PREFIX
           # site_packages; prefer the shorter paths
-          long_prefix = re.compile(r'#{rack}/[0-9\._abrc]+/Frameworks/Python\.framework/Versions/#{xy}/lib/python#{xy}/site-packages')
-          sys.path = [long_prefix.sub('#{HOMEBREW_PREFIX/"lib/python#{xy}/site-packages"}', p) for p in sys.path]
-          # Set the sys.executable to use the opt_prefix, unless explicitly set
-          # with PYTHONEXECUTABLE:
-          if 'PYTHONEXECUTABLE' not in os.environ:
-              sys.executable = '#{opt_bin}/python#{xy}'
+          long_prefix = re.compile(r'#{rack}/[0-9\._abrc]+/Frameworks/Python\.framework/Versions/#{version.major_minor}/lib/python#{version.major_minor}/site-packages')
+          sys.path = [long_prefix.sub('#{HOMEBREW_PREFIX/"lib/python#{version.major_minor}/site-packages"}', p) for p in sys.path]
+          # Set the sys.executable to use the opt_prefix. Only do this if PYTHONEXECUTABLE is not
+          # explicitly set and we are not in a virtualenv:
+          if 'PYTHONEXECUTABLE' not in os.environ and sys.prefix == sys.base_prefix:
+              sys.executable = '#{opt_bin}/python#{version.major_minor}'
     EOS
   end
 
   def caveats
-    xy = if prefix.exist?
-      (prefix/"Frameworks/Python.framework/Versions").children.min.basename.to_s
-    else
-      version.to_s.slice(/(2\.\d)/) || "2.7"
-    end
     <<~EOS
       Python has been installed as
         #{opt_bin}/python2
 
+      Unversioned symlinks `python`, `python-config`, `pip` etc. pointing to
+      `python2`, `python2-config`, `pip2` etc., respectively, have been installed into
+        #{opt_libexec}/bin
+
       You can install Python packages with
         #{opt_bin}/pip2 install <package>
       They will install into the site-package directory
-        #{prefix/"Frameworks/Python.framework/Versions/#{xy}/lib/python#{xy}/site-packages"}
+        #{HOMEBREW_PREFIX/"lib/python#{version.major_minor}/site-packages"}
 
       See: https://docs.brew.sh/Homebrew-and-Python
     EOS
   end
 
   test do
-    xy = (prefix/"Frameworks/Python.framework/Versions").children.min.basename.to_s
     # Check if sqlite is ok, because we build with --enable-loadable-sqlite-extensions
     # and it can occur that building sqlite silently fails if OSX's sqlite is used.
-    system "#{bin}/python#{xy}", "-c", "import sqlite3"
+    system "#{bin}/python#{version.major_minor}", "-c", "import sqlite3"
+
     # Check if some other modules import. Then the linked libs are working.
-    system "#{bin}/python#{xy}", "-c", "import Tkinter; root = Tkinter.Tk()"
-    system "#{bin}/python#{xy}", "-c", "import gdbm"
-    system "#{bin}/python#{xy}", "-c", "import zlib"
-    system "#{bin}/python#{xy}", "-c", "import hashlib"
-    system "#{bin}/python#{xy}", "-c", "import ssl"
+    system "#{bin}/python#{version.major_minor}", "-c", "import gdbm"
+    system "#{bin}/python#{version.major_minor}", "-c", "import hashlib"
+    system "#{bin}/python#{version.major_minor}", "-c", "import ssl"
+    system "#{bin}/python#{version.major_minor}", "-c", "import zlib"
+    on_macos do
+      # Temporary failure on macOS 11.1 due to https://bugs.python.org/issue42480
+      # Reenable unconditionnaly once Apple fixes the Tcl/Tk issue
+      system "#{bin}/python#{version.major_minor}", "-c", "import Tkinter; root = Tkinter.Tk()" if MacOS.full_version < "11.1"
+    end
+
     system bin/"pip2", "list", "--format=columns"
   end
 end
